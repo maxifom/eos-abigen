26b0fb3dca4a98bdce2572347d5cd352469dc08f
Maxim Fominykh <fominykh.max@gmail.com>
2022-03-25 00:26:51 +0500

    add correct type mapping to typescript

--- templates/ts/table_rows.go
+++ templates/ts/table_rows.go
@@ -2,8 +2,14 @@ package ts
 
 import "strings"
 
-var TableRowsTemplate = strings.TrimSpace(`export interface {{.TableName}}Rows {
+var TableRowsTemplate = strings.TrimSpace(`export type {{.TableName}}Rows = {
     more: boolean;
     next_key: string;
     rows: {{.TableName}}[];
-}`)
+};
+export type {{.TableName}}RowsInterm = {
+    more: boolean;
+    next_key: string;
+    rows: {{.TableName}}Interm[];
+};
+`)
